d011ae4dd2512e15b0e0607b1f255359c94d1c73
nqnzp <eqWUYFyRx9fhVvPOaptT>
2019-01-17 14:39:55 -0500

    Minor cleanup

--- blueintel-pull.py
+++ blueintel-pull.py
@@ -12,8 +12,8 @@ stores them in a temporary (date and timestamp) directory for processing.
 
 from O365 import MSGraphProtocol, Connection, Folder, MailBox, Message, MessageAttachment, Account
 from pathlib import Path
-import re
 from configparser import RawConfigParser
+from re import search
 
 parser = RawConfigParser()
 parser.read('credentials.ini')
@@ -26,11 +26,10 @@ my_protocol = MSGraphProtocol()
 my_credentials = (client_id, client_secret)
 my_scopes=['basic', 'mailbox', 'mailbox_shared', 'users', 'message_all', 'message_all_shared']
 
-my_account = Account(credentials=my_credentials, protocol=my_protocol')
+my_account = Account(credentials=my_credentials, protocol=my_protocol)
 
 if not my_account.connection.check_token_file():
     my_account.authenticate(scopes=my_scopes)
-    #my_account.connection.get_authorization_url(requested_scopes=my_scopes)
 else:
     my_account.connection.get_session(token_path=token_file)
 
@@ -44,6 +43,6 @@ for message in folder_messages:
     attachments = message.attachments
     for attachment in attachments:
         pattern = '.csv$'
-        is_csv = re.search(pattern,str(attachment))
+        is_csv = search(pattern,str(attachment))
         if is_csv:
             attachment.save(location='.\\attachments\\')
